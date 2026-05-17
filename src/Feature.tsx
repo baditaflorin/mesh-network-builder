61c9b3e912900f324ce3b02f5bbf5bb9b7a4ec0b
Florin Badita <baditaflorin@gmail.com>
2026-05-17 20:58:58 +0300

    chore: rebuild docs after mesh-common v0.4.0 (UI primitives + 52 MeshNameInput refactors + dedupe-inline fix)

--- src/Feature.tsx
+++ src/Feature.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import {
+  MeshNameInput,
   QRExchange,
   makeScanPayload,
   type MeshConfig,
@@ -131,13 +132,12 @@ function Body({ room, config }: { room: YRoom; config: MeshConfig }) {
         </p>
       </header>
 
-      <input
+      <MeshNameInput
         className="viral-name"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={setName}
         placeholder="your name"
         maxLength={48}
-        aria-label="your name"
       />
 
       <QRExchange
